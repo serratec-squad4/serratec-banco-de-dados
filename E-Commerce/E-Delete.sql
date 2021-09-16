487f427d6707f275aa077682cb70a1a2d685e5e5
AndrePS <aps.teresopolis@gmail.com>
2021-09-16 16:09:46 -0300

    Update

--- E-Commerce/E-Delete.sql
+++ E-Commerce/E-Delete.sql
@@ -1,6 +1,1 @@
-<<<<<<< HEAD
-6.-- 6. Um comando SQL de exclusão de algum registro em uma tabela
-=======
-6.-- 6. Um comando SQL de exclusão de algum registro em uma tabela
->>>>>>> a3f1dfebc0cd68428b8ce0c7230fa43986ae00e9
 DELETE FROM categoria where id = 5
